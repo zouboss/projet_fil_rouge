a397d16a07126426eccbf791f7117aaf8b39a3dc
zouboss <105996357+zouboss@users.noreply.github.com>
2025-05-08 17:34:18 +0000

    Update settings.py

--- Backend/odc/odc/settings.py
+++ Backend/odc/odc/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'odc_db',
         'USER': 'postgres',
         'PASSWORD': 'zou123',
-        'HOST': 'db',
+        'HOST': os.environ.get("DB_HOST", "postgres-service"),
         'PORT': '5432',
     }
 }

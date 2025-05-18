77b2b1fd4dce61863cec17558922f5a0c2b6520d
zouboss <alassanebenzecoly@gmail.com>
2025-05-18 19:11:32 +0000

    gerer les fichiers

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,48 @@
 provider "kubernetes" {
-  config_path = pathexpand("~/.kube/config")
+  config_path = "~/.kube/config"
 }
 
+# SERVICES
+resource "kubernetes_manifest" "postgres_service" {
+  manifest = yamldecode(file("${path.module}/k8s/postgres-service.yaml"))
+}
+
+resource "kubernetes_manifest" "backend_service" {
+  manifest = yamldecode(file("${path.module}/k8s/backend-service.yaml"))
+}
+
+resource "kubernetes_manifest" "frontend_service" {
+  manifest = yamldecode(file("${path.module}/k8s/frontend-service.yaml"))
+}
+
+# DEPLOYMENTS
 resource "kubernetes_manifest" "postgres" {
-  manifest = yamldecode(file("${path.module}/k8s/postgres.yaml"))
+  manifest = yamldecode(file("${path.module}/k8s/postgres-statefulset.yaml"))
+  depends_on = [kubernetes_manifest.postgres_service]
 }
 
 resource "kubernetes_manifest" "backend" {
-  manifest = yamldecode(file("${path.module}/k8s/backend.yaml"))
-  depends_on = [kubernetes_manifest.postgres]
-}
-
-resource "kubernetes_manifest" "migrate_job" {
-  manifest = yamldecode(file("${path.module}/k8s/migrate-job.yaml"))
-  depends_on = [kubernetes_manifest.backend]
+  manifest = yamldecode(file("${path.module}/k8s/backend-deployment.yaml"))
+  depends_on = [kubernetes_manifest.backend_service]
 }
 
 resource "kubernetes_manifest" "frontend" {
-  manifest = yamldecode(file("${path.module}/k8s/frontend.yaml"))
-  depends_on = [kubernetes_manifest.migrate_job]
+  manifest = yamldecode(file("${path.module}/k8s/frontend-deployment.yaml"))
+  depends_on = [kubernetes_manifest.frontend_service]
 }
 
+# JOB DE MIGRATION
+resource "kubernetes_manifest" "migrate_job" {
+  manifest = yamldecode(file("${path.module}/k8s/migrate-job.yaml"))
+  depends_on = [kubernetes_manifest.postgres]
+}
+
+# INGRESS
 resource "kubernetes_manifest" "ingress" {
   manifest = yamldecode(file("${path.module}/k8s/ingress.yaml"))
-  depends_on = [kubernetes_manifest.frontend]
+  depends_on = [
+    kubernetes_manifest.frontend_service,
+    kubernetes_manifest.backend_service
+  ]
 }
 
